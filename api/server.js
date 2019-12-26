2b4696657075c535881e091ecbc3c2993fcf3889
Kerri-Ann <kaybee1jam@yahoo.com>
2019-12-25 21:38:46 -0800

    implemented helmet to hide headers

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const server = express();
+const helmet = require('helmet')
+
+// Routes
 const userRoute = require('../users/users-router');
 const authRoute = require('../auth/auth-router');
 const exerciseRoute = require('../exercises/exercise-router');
 
 server.use(express.json());
+server.use(helmet());
 server.use('/api/users', userRoute);
 server.use('/api/auth', authRoute);
 server.use('/api/exercises', exerciseRoute);
