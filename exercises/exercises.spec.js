22c45cff8133c8d0649e6f77f1c87df177bc4f14
Kerri-Ann <kaybee1jam@yahoo.com>
2020-01-07 13:04:25 -0500

    add test for deleting an exercise

--- exercises/exercises.spec.js
+++ exercises/exercises.spec.js
@@ -1,20 +1,57 @@
 const db = require('../database/db-config');
 const Exercises = require('./exercise-models');
 
-describe('exercises route', () => {
-   it('add exercise', async () => { 
-
-      await Exercises.addExercise({
-         "user_id": 1,
-         "name": "jumping jacks",
-         "amount_lifted": 0,
-         "body_region": "abs, legs"
-      });
-
-      const exercises = await db('exercises');
-      const exerciseCount = await db('exercises').then(exercises => exercises.length);
-
-      expect(exercises).toHaveLength(exerciseCount);
-      
+describe('exercises model', () => {
+   beforeEach(async () => {
+      await db('exercises').truncate();
    });
-})
+
+   // Add an exercise.
+   describe('addExercise()', function () {
+      it('add exercise', async function () {
+
+         await Exercises.addExercise({
+            "user_id": 1,
+            "name": "jumping jacks",
+            "amount_lifted": 0,
+            "body_region": "abs, legs"
+         });
+
+         const exercises = await db('exercises');
+         const exerciseCount = await db('exercises').then(exercises => exercises.length);
+
+         expect(exercises).toHaveLength(exerciseCount);
+
+      });
+   });
+
+   // Delete an exercise.
+   describe('deleteExercise()', function () {
+      it('should delete an exercise', async function () {
+         await Exercises.addExercise({
+            "user_id": 1,
+            "name": "jumping jacks",
+            "amount_lifted": 0,
+            "body_region": "abs, legs"
+         });
+         await Exercises.addExercise({
+            "user_id": 1,
+            "name": "Bench press",
+            "amount_lifted": 0,
+            "body_region": "arms"
+         });
+         await Exercises.addExercise({
+            "user_id": 1,
+            "name": "squats",
+            "amount_lifted": 0,
+            "body_region": "legs"
+         });
+
+         const exerciseCount = await db('exercises').then(exercises => exercises.length);
+
+         await Exercises.deleteExercise(3);
+         const exercises = await db('exercises');
+         expect(exercises).toHaveLength(exerciseCount - 1);
+      });
+   });
+});
