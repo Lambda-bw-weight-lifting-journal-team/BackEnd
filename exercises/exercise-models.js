d2cf592b9400b407da16d62a0199965643b9d221
Kerri-Ann <kaybee1jam@yahoo.com>
2020-01-08 19:56:26 -0500

    update exercise model

--- exercises/exercise-models.js
+++ exercises/exercise-models.js
@@ -28,12 +28,7 @@ function getUserExercises(userId) {
 
 function addExercise(data) {
    return db('exercises')
-      .insert(data)
-      .then(ids => {
-         const [id] = ids;
-         return getExerciseById(id);
-      })
-      // .then(([id]) => getExerciseById(id) );
+      .insert(data);
 }
 
 function getExerciseById(id) { 
